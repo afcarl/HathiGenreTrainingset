dcdec469ea9fd2058d04257ecd66c95b938e521d
tedunderwood <tunder@illinois.edu>
2014-10-28 22:45:47 -0500

    Bounding output of smooth.R to 0 < x < 1.

--- confidence/smooth.R
+++ confidence/smooth.R
@@ -1,10 +1,19 @@
 # smooth precision-recall curves
 library(zoo)
 # we need the locf function
+
+bounded <- function(avector) {
+  avector[avector < 0] <- 0
+  avector[avector > 1] <- 1
+  avector
+}
+
 conf80 <- read.csv('/Users/tunder/output/confidence80.csv')
 conf80 <- na.locf(conf80)
 for (i in 1:4) {
   conf80[[i]] <- lowess(conf80[[i]], f = .3)$y
 }
+
+conf80 <- lapply(conf80, bounded)
 
 write.csv(conf80, file = '/Users/tunder/Dropbox/PythonScripts/training/alldata/confidence/smoothed.csv', row.names = FALSE)
