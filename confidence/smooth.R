2c6016c98671770ef51c377195b8397d952cdb98
tedunderwood <tunder@illinois.edu>
2014-11-01 16:35:44 -0500

    Limiting significant digits in output.

--- confidence/smooth.R
+++ confidence/smooth.R
@@ -10,10 +10,10 @@ bounded <- function(avector) {
 
 conf80 <- read.csv('/Users/tunder/output/confidence80.csv')
 conf80 <- na.locf(conf80)
-for (i in 1:4) {
-  conf80[[i]] <- lowess(conf80[[i]], f = .3)$y
+for (i in 1:8) {
+  firstpass <- lowess(conf80[[i]], f = .2)$y
+  secondpass <- bounded(firstpass)
+  conf80[[i]] <- signif(secondpass, digits = 3)
 }
-
-conf80 <- lapply(conf80, bounded)
 
 write.csv(conf80, file = '/Users/tunder/Dropbox/PythonScripts/training/alldata/confidence/smoothed.csv', row.names = FALSE)
